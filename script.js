913f1d160e60f5fab2e6bd4dff41df4618238664
unknown <laetitiaroche53@gmail.com>
2018-10-25 21:06:11 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// var ingredientsApplePie = ["Flour", "Sugar", "Butter", "Apple", "Honey"]
+
+var recette = [
+    {name: 'Apple Pie', ingredients: [{ name : "Flour", image : "'./images/flour.png'"}, 
+                                    {name : "Sugar", image : "Butter", "Apple", "Honey"] },
+    {name: 'Burger',    ingredients: ["Bread", "Steack", "Salad", "Tomato", "Cheese"] },
+];   
+
+var image = [
+    Flour:  './images/flour.png',
+    Sugar: '/images/sugar.jpg',
+    Butter: '/images/butter.png',
+    Apple: '/images/apple.jpg',
+    Honey:'/images/honey.jpg',
+];
+
