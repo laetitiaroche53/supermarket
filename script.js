5d07e7f0082fb02fd226e880914fb681a461e2f9
unknown <laetitiaroche53@gmail.com>
2018-10-25 21:31:29 +0200

    secondt commit

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 
-// var ingredientsApplePie = ["Flour", "Sugar", "Butter", "Apple", "Honey"]
-
 var recette = [
-    {name: 'Apple Pie', ingredients: [{ name : "Flour", image : "'./images/flour.png'"}, 
-                                    {name : "Sugar", image : "Butter", "Apple", "Honey"] },
-    {name: 'Burger',    ingredients: ["Bread", "Steack", "Salad", "Tomato", "Cheese"] },
+    {recetteName: 'Apple Pie', ingredients: [{ name : "Flour", image : "./images/flour.png"}, 
+                                     { name : "Sugar", image : "./images/sugar.jpg"},
+                                     { name : "Butter", image : "./images/butter.png" },
+                                     { name : "Apple", image : "./images/apple.jpg" },
+                                     { name : "Honey", image : "./images/honey.jpg"},
+    {recetteName: 'Burger',    ingredients: ["Bread", "Steack", "Salad", "Tomato", "Cheese"]},
 ];   
 
-var image = [
-    Flour:  './images/flour.png',
-    Sugar: '/images/sugar.jpg',
-    Butter: '/images/butter.png',
-    Apple: '/images/apple.jpg',
-    Honey:'/images/honey.jpg',
-];
+//fonction pour générer aléatoirement une des recettes ??
+function randomSelector(recette){
+    var keysArr = Object.keys(recette);
+    var numKeys = keysArr.length;
+    var rndIndx = Math.floor(Math.random() * numKeys);
+    return recette[keysArr[rdnIndx]];
+    console.log(recette[keysArr[rdnIndx]];);
+};
+
+
+
 
